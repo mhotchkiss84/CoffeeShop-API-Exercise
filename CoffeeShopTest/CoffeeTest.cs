292812ef297743b4083efce52af08fe4bc9bc398
Mike Hotchkiss <michael.hotchkiss@protonmail.com>
2020-08-13 11:42:27 -0400

    Fixed issues with the format

---
+++ CoffeeShopTest/CoffeeTest.cs
@@ -0,0 +1,14 @@
+//using System;
+//using Xunit;
+
+//namespace CoffeeShopTest
+//{
+//    public class CoffeeTest
+//    {
+//        [Fact]
+//        public void Test1()
+//        {
+
+//        }
+//    }
+//}
